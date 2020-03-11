8ac5b6fdab84f3b66945913ab9cdd875cf67256b
Milind Vaidya <milindv@uber.com>
2020-03-11 13:49:22 -0700

    Separate method to add up areas

--- interfaceShape.go
+++ interfaceShape.go
@@ -27,18 +27,24 @@ type Shape interface {
 	Area() float64
 }
 
+func TotalArea(s []Shape) float64 {
+
+	total := 0.0
+
+	for _, currShape := range s {
+		total += currShape.Area()
+	}
+
+	return total
+}
+
 func main() {
 
 	// Remember : always use pointers when internal details change
 	s := &Square{5}
 	c := &Circle{3, 4, 5}
 	shapeSlice := []Shape{s, c}
-	total := 0.0
-
-	for _, currShape := range shapeSlice {
-		total += currShape.Area()
-	}
-
+	total := TotalArea(shapeSlice)
 	fmt.Printf("Total area for all shapes in the slice : %f\n", total)
 
 }
