17e23756a4a157c37fbb811fd52776edac387d78
Milind Vaidya <milindv@uber.com>
2020-03-10 23:19:42 -0700

    Using GoFmt

--- interfaceShape.go
+++ interfaceShape.go
@@ -1,43 +1,44 @@
 package main
+
 import (
-    "fmt"
-    "math"
+	"fmt"
+	"math"
 )
 
 type Square struct {
-    Side float64
+	Side float64
 }
 
-func(s *Square) Area() float64 {
-    return s.Side * s.Side
+func (s *Square) Area() float64 {
+	return s.Side * s.Side
 }
 
 type Circle struct {
-    Centerx float64
-    Centery float64
-    Radius float64
+	Centerx float64
+	Centery float64
+	Radius  float64
 }
 
 func (c *Circle) Area() float64 {
-    return math.Pi * c.Radius * c.Radius
+	return math.Pi * c.Radius * c.Radius
 }
 
 type Shape interface {
-    Area() float64
+	Area() float64
 }
 
 func main() {
 
-    // Remember : always use pointers when internal details change
-    s := &Square{5}
-    c := &Circle{3,4,5}
-    shapeSlice := [] Shape{s,c}
-    total := 0.0
+	// Remember : always use pointers when internal details change
+	s := &Square{5}
+	c := &Circle{3, 4, 5}
+	shapeSlice := []Shape{s, c}
+	total := 0.0
 
-    for _, currShape := range shapeSlice {
-        total += currShape.Area()
-    }
+	for _, currShape := range shapeSlice {
+		total += currShape.Area()
+	}
 
-    fmt.Printf("Total area for all shapes in the slice : %f\n", total)
+	fmt.Printf("Total area for all shapes in the slice : %f\n", total)
 
 }
